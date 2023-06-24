7f1157ff6d57ef9d94459d7c1abb3bf2ceb71459
LongBong <vinay.bayyapunedi@gmail.com>
2023-06-24 17:26:57 +1000

    Add project files.

---
+++ Assignment 1/Account.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace Assignment_1
+{
+    class Account
+    {
+        private int accountNumber;
+        private string firstName;
+        private string lastName;
+        private string address;
+        private int phone;
+        private string email;
+        private int balance;
+        private string[] accountInformation = new string[7];
+        Random number = new Random();
+
+        //public string accountPath = @"c:\Users\vinay\OneDrive\Documents\Visual Studio 2019\UTS Code Files\Assignment 1\Assignment 1";
+        // created the path for the file to be stored in
+
+        public Account(string firstName, string lastName, string address, int phone, string email, int balance)
+        {
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.phone = phone;
+            this.email = email;
+            this.balance = balance;
+        }
+
+        public void createAccount()
+        {
+
+            this.accountNumber = number.Next(100000, 10000000);
+
+            string accountFile = this.accountNumber + ".txt";
+            //created the file name 
+
+            string accountPath = @"c:\Users\vinay\OneDrive\Documents\Visual Studio 2019\UTS Code Files\Assignment 1\Assignment 1";
+
+            accountPath = System.IO.Path.Combine(accountPath, accountFile);
+            // created a file with name <accountnumber>.txt
+
+            
+            accountInformation[0] = "AccountNo|" + this.accountNumber;
+            accountInformation[1] = "First Name|" + this.firstName;
+            accountInformation[2] = "Last Name|" + this.lastName;
+            accountInformation[3] = "address|" + this.address;
+            accountInformation[4] = "Phone|" + this.phone;
+            accountInformation[5] = "Email|" + this.email;
+            accountInformation[6] = "Balance| " + this.balance;
+
+            Console.WriteLine(accountInformation[6]);
+
+            System.IO.File.WriteAllLines(accountPath, accountInformation);
+
+            //saved all the information with proper format       
+      
+
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress("banking.system@gmail.com");
+
+            try
+            {
+                mail.To.Add(this.email);
+            }
+            catch (System.FormatException fe)
+            {
+                Console.WriteLine("The email format is wrong", fe.Source);
+            }
+
+
+            mail.Subject = "Account details";
+
+            Attachment accountData = new Attachment(accountPath);
+            mail.Attachments.Add(accountData);
+
+            SmtpClient client = new SmtpClient("smtp.live.com", 587);
+            client.Credentials = System.Net.CredentialCache.DefaultNetworkCredentials;
+
+            Console.WriteLine("Account Created! details will be provided via email." + "\n");
+
+            try
+            {
+                client.Send(mail);
+            }
+            catch
+            {
+                Console.WriteLine("The email was not sent" + "\n");
+            }
+
+            Console.WriteLine("Account number is: " + this.accountNumber + "\n");
+        }
+
+        public static string searchAccount(int accountNumber)
+        {
+
+            Boolean accountChecker = false;
+
+            string accountPath = @"c:\Users\vinay\OneDrive\Documents\Visual Studio 2019\UTS Code Files\Assignment 1\Assignment 1";
+
+            DirectoryInfo di = new DirectoryInfo(@"c:\Users\vinay\OneDrive\Documents\Visual Studio 2019\UTS Code Files\Assignment 1\Assignment 1");
+            FileInfo[] files = di.GetFiles("*.txt");
+
+            ArrayList accountFiles = new ArrayList();
+
+            string[] accountNos;
+
+            ArrayList accountNumbers = new ArrayList();
+
+            foreach (FileInfo file in files)
+            {
+                accountFiles.Add(file.Name);
+            }
+
+            foreach (string accountFile in accountFiles)
+            {
+                accountNos = accountFile.Split(".");
+
+                accountNumbers.Add(accountNos[0]);
+            }
+
+            foreach (string accountNo in accountNumbers)
+            {
+
+                int account = Convert.ToInt32(accountNo);
+                if (account == accountNumber)
+                {
+                    accountChecker = true;
+                }
+            }
+
+            if (accountChecker == true)
+            {
+                Console.WriteLine("Account found!" + "\n");
+                string accountFile = accountNumber + ".txt";
+                accountPath = System.IO.Path.Combine(accountPath, accountFile);
+            }
+            else
+            {
+                accountPath = "wrong";
+            }
+
+            return accountPath;
+        }
+
+        public static void deposit(string accountPath, string[] accountInformation, int accountNumber, int depositAmount)
+        {
+
+            string[] balanceInfo;
+
+            balanceInfo = accountInformation[accountInformation.Length - 1].Split("|");
+
+            int originalBalance = Convert.ToInt32(balanceInfo[1]);
+
+            int newBalance = originalBalance + depositAmount;
+
+            DateTime dt = DateTime.Now;
+
+            string[] lines = new string[2];
+            
+            try
+            {
+                StreamWriter sw = System.IO.File.AppendText(accountPath);
+
+                sw.WriteLine(dt.ToString() + "|Deposit|" + originalBalance + "|" + newBalance);
+
+                sw.WriteLine("New Balance|" + newBalance);
+
+                sw.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Close the application and deposit the money" +
+                " again into this account. The method is not working when the" +
+                " user creates an account and runs the deposit method is run" +
+                " right after it.", ex.Source);
+                return;
+            }
+
+            Console.WriteLine("Deposit Successful" + "\n");
+        }
+
+        public static void withdraw(string accountPath, string[] accountInformation, int accountNumber, int withdrawAmount)
+        {
+            string[] balanceInfo;
+
+            balanceInfo = accountInformation[accountInformation.Length - 1].Split("|");
+
+            int originalBalance = Convert.ToInt32(balanceInfo[1]);
+
+            int newBalance = originalBalance - withdrawAmount;
+
+            DateTime dt = DateTime.Now;
+
+            try
+            {
+                StreamWriter sw = System.IO.File.AppendText(accountPath);
+
+                sw.WriteLine(dt.ToString() + "|Withdraw|" + originalBalance + "|" + newBalance);
+
+                sw.WriteLine("New Balance|" + newBalance);
+
+                sw.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Close the application and withdraw the money" +
+                " again into this account. The method is not working when the" +
+                " user creates an account and runs the deposit method is run" +
+                " right after it.", ex.Source);
+                return;
+            }
+
+            Console.WriteLine("Withdraw Successful" + "\n");
+        }
+
+        public static void showStatement(int accountNumber)
+        {
+            string accountPath = Account.searchAccount(accountNumber);
+
+            string[] accountInformation = System.IO.File.ReadAllLines(accountPath);
+
+            Console.WriteLine("ACCOUNT STATEMENT" + "\n");
+
+            string[] singleInformation;
+
+            foreach (string accountInfo in accountInformation)
+            {
+                singleInformation = accountInfo.Split("|");
+                Console.WriteLine(singleInformation[0] + ": " + singleInformation[1]);
+            }
+
+            Console.WriteLine("\n");
+        }
+
+        public static void deleteAccount(int accountNumber, string accountPath)
+        {
+
+            Console.WriteLine("The details are displayed below" + "\n");
+
+            string[] accountInformation = System.IO.File.ReadAllLines(accountPath);
+
+            string[] singleInformation;
+
+            foreach (string accountInfo in accountInformation)
+            {
+                singleInformation = accountInfo.Split("|");
+                Console.WriteLine(singleInformation[0] + ": " + singleInformation[1]);
+            }
+
+            Console.Write("deleteAccount(y / n)? ");
+
+            string yesNo = Console.ReadLine();
+
+            if (yesNo == "y" || yesNo == "Y")
+            {
+
+               try
+                {
+                    File.Delete(accountPath);
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("Close the application and delete the account." +
+                    " The method is not working when the user creates an account and" +
+                    " deletes the account right after", ex.Source);
+                    return;
+                }
+                Console.WriteLine("\n" + "Account Deleted!" + "\n");
+            }
+            else
+            {
+                Console.WriteLine("Account is not deleted");
+                return;
+            }
+
+
+        }
+    }
+}
---
+++ Assignment 1/Program.cs
@@ -0,0 +1,541 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\vinay\OneDrive\Documents\Visual Studio 2019\UTS Code Files\Assignment 1\Assignment 1\login.txt");
+            // ADD FILENOTFOUND EXCEPTION FOR THIS 
+
+            ArrayList usernames = new ArrayList(); //stores usernames
+
+            ArrayList passwords = new ArrayList(); //stores passwords
+
+            int menuCount = 0;
+            Boolean menuChecker = true;
+
+            string inputUsername;
+            string inputPassword;
+
+            Boolean usernameChecker = false;
+            Boolean passwordChecker = false;
+
+            int usernameCount = 0;
+            int passwordCount = 0;
+
+            int usernameError = 0;
+            int passwordError = 0;
+
+            foreach (string line in lines)
+            {
+
+                string[] words = line.Split('|');
+
+                usernames.Add(words[0]);
+                passwords.Add(words[1]);
+                
+            }
+
+            Console.WriteLine(" WELCOME TO SIMPLE BANKING SYSTEM " + "\n");
+
+            Console.WriteLine(" LOGIN TO START ");
+
+            // For login username and password are to be crosschecked with the
+            // valid credentials in login.txt
+
+
+            while(usernameChecker == false)
+            {
+
+                if (usernameError > 0)
+                {
+                    Console.WriteLine("You enterned an invalid username");
+                }
+
+                Console.Write(" User Name: ");
+
+                inputUsername = Console.ReadLine();
+
+                foreach (string username in usernames)
+                {
+                    if (inputUsername == username)
+                    {
+                        usernameCount = usernames.IndexOf(username);
+                        usernameChecker = true;
+                    }
+                }
+
+                usernameError++;
+            }
+
+            while (passwordChecker == false)
+            {
+
+                if (passwordError > 0)
+                {
+                    Console.WriteLine("You enterned an invalid password");
+                }
+
+                Console.Write(" Password: ");
+
+                inputPassword = ReadPassword();
+
+                foreach (string password in passwords)
+                {
+
+                    passwordCount = passwords.IndexOf(password);
+                    if (passwordCount == usernameCount && inputPassword == password)
+                    {
+                        passwordChecker = true;
+                    }
+                }
+
+                passwordError++;
+            }
+
+            while(menuChecker == true)
+            {
+
+                if(menuCount > 0)
+                {
+                    Console.WriteLine("Press any key to continue" + "\n");
+                    Console.ReadKey();
+                }
+
+                Console.WriteLine("\n" + "WELCOME TO SIMPLE BANKING SYSTEM" + "\n");
+                Console.WriteLine("1. Create a new account");
+                Console.WriteLine("2. Search for an account");
+                Console.WriteLine("3. Deposit");
+                Console.WriteLine("4. Withdraw");
+                Console.WriteLine("5. A/C statement");
+                Console.WriteLine("6. Delete account");
+                Console.WriteLine("7. Exit" + "\n");
+
+                Console.WriteLine("Enter your choice (1 - 7): ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        createAccount();
+                        break;
+
+                    case "2":
+                        searchAccount();
+                        break;
+
+                    case "3":
+                        deposit();
+                        break;
+
+                    case "4":
+                        withdraw();
+                        break;
+
+                    case "5":
+                        showStatement();
+                        break;
+
+                    case "6":
+                        deleteAccount();
+                        break;
+
+                    case "7":
+                        menuChecker = false;
+                        return;
+                }
+
+                menuCount++;
+            }
+
+
+
+
+
+        }
+
+        public static void createAccount()
+        {
+
+
+            string yesNo;
+            Boolean informationCheck = false;
+            int informationCount = 0;
+
+            int phoneCount = 0;
+            Boolean phoneChecker = false;
+            int phone = 0;
+
+            int emailCount = 0;
+            Boolean emailChecker = false;
+            string email = "";
+
+            string firstName = "";
+            string lastName = "";
+            string address = "";
+
+            int balance = 0;
+
+
+
+            Console.WriteLine("\n" + "CREATE A NEW ACCOUNT" + "\n");
+            Console.WriteLine("ENTER THE DETAILS" + "\n");
+
+            while (informationCheck == false)
+            {
+
+                if (informationCount > 0)
+                {
+                    Console.WriteLine("Enter your account information again" + "\n");
+                    phoneChecker = false;
+                    emailChecker = false;
+                }
+
+                Console.Write("First Name: ");
+                firstName = Console.ReadLine();
+                Console.Write("Last Name: ");
+                lastName = Console.ReadLine();
+                Console.Write("Address: ");
+                address = Console.ReadLine();
+
+
+                while (phoneChecker == false)
+                {
+
+                    if (phoneCount > 0)
+                    {
+                        Console.WriteLine("The phone number should not be more than 10 characters");
+                    }
+
+                    Console.Write("Phone: ");
+                    string phoneNumber = Console.ReadLine();
+
+                    if (phoneNumber.Length <= 10)
+                    {
+                        phoneChecker = true;
+                    }
+
+                    if (phoneChecker == true)
+                    {
+                        phone = Convert.ToInt32(phoneNumber);
+                    }
+
+                    phoneCount++;
+                }
+
+
+                while (emailChecker == false)
+                {
+
+                    if (emailCount > 0)
+                    {
+                        Console.WriteLine("The entered string should have an @ signature to be a valid email ");
+                    }
+
+                    Console.Write("Email: ");
+                    email = Console.ReadLine();
+
+                    if (email.Contains('@'))
+                    {
+                        emailChecker = true;
+                    }
+
+                    emailCount++;
+                }
+
+                Console.Write("Balance: ");
+                balance = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Is the information correct (y/n)? ");
+                yesNo = Console.ReadLine();
+
+                if (yesNo == "Y" || yesNo == "y")
+                {
+                    informationCheck = true;
+                }
+
+                informationCount++;
+            }
+
+            Account account = new Account(firstName, lastName, address, phone, email, balance);
+            account.createAccount();
+
+
+
+        }
+
+        public static void searchAccount()
+        {
+
+            string accountPath = "";
+
+            Console.WriteLine("SEARCH FOR AN ACCOUNT" + "\n");
+            Console.WriteLine("ENTER THE DETAILS" + "\n");
+
+            int accountNumber = 0;
+            Boolean accountChecker = true;
+
+            while (accountChecker)
+            {
+
+                Console.Write("Account Number: ");
+
+                try
+                {
+                    accountNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    return;
+                }
+
+
+                accountPath = Account.searchAccount(accountNumber);
+
+                if (accountPath != "wrong")
+                {
+                    accountChecker = false;
+                }
+                else
+                {
+                    Console.WriteLine("The account number entered was wrong. Enter the account number again!");                }
+            }
+
+
+
+            Console.WriteLine("ACCOUNT DETAILS" + "\n");
+
+            string[] accountInformation = System.IO.File.ReadAllLines(accountPath);
+
+            foreach (string accountInfo in accountInformation)
+            {
+                Console.WriteLine(accountInfo);
+            }
+
+            Console.WriteLine("\n");
+
+        }
+
+        public static void deposit()
+        {
+
+            string accountPath = "";
+
+            Console.WriteLine("DEPOSIT" + "\n");
+            Console.WriteLine("ENTER THE DETAILS" + "\n");
+
+
+            int accountNumber = 0;
+            int depositAmount = 0;
+            Boolean accountChecker = true;
+            Boolean amountChecker = true;
+
+            while (accountChecker)
+            {
+
+                Console.Write("Account Number: ");
+
+                try
+                {
+                    accountNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    return;
+                }
+
+                accountPath = Account.searchAccount(accountNumber);
+
+                if (accountPath != "wrong")
+                {
+                    accountChecker = false;
+                }
+                else
+                {
+                    Console.WriteLine("The account number entered was wrong. Enter the account number again!");
+                }
+            }
+
+            while (amountChecker)
+            {
+                Console.Write("Amount: ");
+
+                try
+                {
+                    depositAmount = Convert.ToInt32(Console.ReadLine());
+                    amountChecker = false;
+                }
+                catch
+                {
+                    Console.WriteLine("The deposit amount provided is not in the right format");
+                }
+            }
+
+            string[] accountInformation = System.IO.File.ReadAllLines(accountPath);
+
+            Account.deposit(accountPath, accountInformation, accountNumber, depositAmount);
+
+        }
+
+        public static void withdraw()
+        {
+
+            string accountPath = "";
+
+            Console.WriteLine("WITHDRAW" + "\n");
+            Console.WriteLine("ENTER THE DETAILS" + "\n");
+
+            int accountNumber = 0;
+            int withdrawAmount = 0;
+            Boolean accountChecker = true;
+            Boolean amountChecker = true;
+
+            while (accountChecker)
+            {
+
+                Console.Write("Account Number: ");
+
+                try
+                {
+                    accountNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    return;
+                }
+
+                accountPath = Account.searchAccount(accountNumber);
+
+                if (accountPath != "wrong")
+                {
+                    accountChecker = false;
+                }
+                else
+                {
+                    Console.WriteLine("The account number entered was wrong. Enter the account number again!");
+                }
+            }
+
+            while (amountChecker)
+            {
+                Console.Write("Amount: ");
+
+                try
+                {
+                    withdrawAmount = Convert.ToInt32(Console.ReadLine());
+                    amountChecker = false;
+                }
+                catch
+                {
+                    Console.WriteLine("The deposit amount provided is not in the right format");
+                }
+            }
+
+            string[] accountInformation = System.IO.File.ReadAllLines(accountPath);
+
+            Account.withdraw(accountPath, accountInformation, accountNumber, withdrawAmount);
+        }
+
+        public static void showStatement()
+        {
+            string accountPath = "";
+
+            Console.WriteLine("STATEMENT" + "\n");
+            Console.WriteLine("ENTER THE DETAILS" + "\n");
+
+            int accountNumber = 0;
+            Boolean accountChecker = true;
+
+            while (accountChecker)
+            {
+
+                Console.Write("Account Number: ");
+
+                try
+                {
+                    accountNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    return;
+                }
+
+                accountPath = Account.searchAccount(accountNumber);
+
+                if (accountPath != "wrong")
+                {
+                    accountChecker = false;
+                }
+                else
+                {
+                    Console.WriteLine("The account number entered was wrong. Enter the account number again!");
+                }
+            }
+
+            // what if i search the account here and then call the method
+
+            Account.showStatement(accountNumber);
+        }
+
+        public static void deleteAccount()
+        {
+            string accountPath = "";
+
+            Console.WriteLine("DELETE AN ACCOUNT" + "\n");
+            Console.WriteLine("ENTER THE DETAILS" + "\n");
+
+            int accountNumber = 0;
+            Boolean accountChecker = true;
+
+            while (accountChecker)
+            {
+
+                Console.Write("Account Number: ");
+
+                try
+                {
+                    accountNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    return;
+                }
+
+                accountPath = Account.searchAccount(accountNumber);
+
+                if (accountPath != "wrong")
+                {
+                    accountChecker = false;
+                }
+                else
+                {
+                    Console.WriteLine("The account number entered was wrong. Enter the account number again!");
+                }
+            }
+
+            Console.WriteLine("\n");
+
+            Account.deleteAccount(accountNumber, accountPath);
+        }
+
+        public static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo letter = Console.ReadKey(true); 
+            while (letter.Key != ConsoleKey.Enter)
+            {
+                Console.Write("*");
+                password = password + letter.KeyChar;
+                letter = Console.ReadKey(true);
+            }
+
+            return password;
+        }
+    }
+}
